932e7a03baa6b3289909f30c176fdef61add0a68
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-09-18 08:52:18 +0200

    initial commit, hp och namn på spelarna

---
+++ Slagsmålspelet_övning/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int hp1 = 10;
+int hp2 = 10;
+
+Console.WriteLine("Player 1. Please name your Warrior.");
+Console.Write("Name: ");
+string name1 = Console.ReadLine();
+
+Console.WriteLine("Player 2. Please name your warrior.");
+Console.Write("Name: ");
+string name2 = Console.ReadLine();
