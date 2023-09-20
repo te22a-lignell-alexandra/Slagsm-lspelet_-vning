68b17e02165db405e695d569601ef31c3bc24725
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-09-20 11:05:06 +0200

    player1 has 2 attacks

--- Slagsmålspelet_övning/Program.cs
+++ Slagsmålspelet_övning/Program.cs
@@ -69,7 +69,9 @@ while (tryagain == "yes")
             moveon = "yes";
         }
     }
-    // rundorna börjar här
+
+
+    // rundorna börjar här-----------------------------
     while (hp1 > 0 && hp2 > 0)
     {
         // runda nr och HP kvar
@@ -84,18 +86,54 @@ while (tryagain == "yes")
         // .Next eftersom man stoppar in generator talet och får ut nästa tal, typ. 
         // (20) för att ange vad max talet ska vara, annars blir det max värdet som en int kan ha
         // om (20,100) = som minst 20, som max 100
-        int damage1 = generator.Next(20);
-        hp2 -= damage1;
-        hp2 = Math.Max(0, hp2);
-        Console.WriteLine($"{player1} does {damage1} damage to {player2}");
-        Console.ReadLine();
 
-        // skada player 2
-        int damage2 = generator.Next(20);
-        hp1 -= damage2;
-        hp1 = Math.Max(0, hp1);
-        Console.WriteLine($"{player2} does {damage2} damage to {player1}");
-        Console.ReadLine();
+        // int damage1 = generator.Next(20);
+        // hp2 -= damage1;
+        // hp2 = Math.Max(0, hp2);
+        // Console.WriteLine($"{player1} does {damage1} damage to {player2}");
+        // Console.ReadLine();
+
+        // // skada player 2
+        // int damage2 = generator.Next(20);
+        // hp1 -= damage2;
+        // hp1 = Math.Max(0, hp1);
+        // Console.WriteLine($"{player2} does {damage2} damage to {player1}");
+        // Console.ReadLine();
+
+        Console.WriteLine($"{player1}, (A)cast strong but difficult spell or (B)stab lightly with a dagger?");
+        string attack = Console.ReadLine().ToLower();
+        // player1 attack
+        if (attack == "a")
+        {
+            int hitormiss1a = generator.Next(100);
+
+            if (hitormiss1a > 60)
+            {
+                int damage1 = generator.Next(30,60);
+                hp2 -= damage1;
+                Console.WriteLine($"HIT. {player1} does {damage1} damage to {player2}.");
+            }
+            else
+            {
+                Console.WriteLine($"MISS. {player1} does no damage to {player2}.");
+            }
+        if (attack == "b")
+        {
+            int hitormiss1b = generator.Next(100);
+
+            if (hitormiss1b > 20)
+            {
+                int damage2 = generator.Next(10, 30);
+                hp2 -= damage2;
+                Console.WriteLine($"HIT. {player2} does {damage2} damage to {player1}.");
+            }
+            else
+            {
+                Console.WriteLine($"MISS. {player2} does no damage to {player1}.");
+            }
+        }
+        }
+
     }
 
 
