d8773a6a2d2b88cc59ca36873f4892787221140f
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2023-09-20 10:50:01 +0200

    Namn kan inte vara kortare än 3 eller längre än 10

--- Slagsmålspelet_övning/Program.cs
+++ Slagsmålspelet_övning/Program.cs
@@ -1,36 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
+
+
 
 int hp1 = 100;
 int hp2 = 100;
 string tryagain = "yes";
 
+// generator = variabel som kan innehålla en slumpgenerator
 Random generator = new Random();
 
 while (tryagain == "yes")
 {
-int round = 0;
+    int round = 0;
 
-// namn
-Console.WriteLine("Player 1. Please name your Warrior.");
-Console.Write("Name: ");
-string player1 = Console.ReadLine();
 
-Console.WriteLine("Player 2. Please name your warrior.");
-Console.Write("Name: ");
-string player2 = Console.ReadLine();
+    // namn
+    Console.WriteLine("Player 1. Please name your Warrior.");
+    Console.Write("Name: ");
+    string player1 = Console.ReadLine();
 
-    while (hp1 > 0  && hp2 > 0)
+
+    string moveon = "no";
+    while (moveon == "no")
+    {
+        // player1.Length = en int för antalet bokstäver i stringen player1
+        if (player1.Length < 3)
+        {
+            Console.WriteLine("The name cannot be shorter than 3 characters. Try again.");
+            Console.Write("Name: ");
+            player1 = Console.ReadLine();
+        }
+        else if (player1.Length > 10)
+        {
+            Console.WriteLine("The name cannot be longer than 10 characters. Try again.");
+            Console.Write("Name: ");
+            player1 = Console.ReadLine();
+        }
+        else
+        {
+            moveon = "yes";
+        }
+    }
+
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("Player 2. Please name your warrior.");
+    Console.Write("Name: ");
+    string player2 = Console.ReadLine();
+
+
+    moveon = "no";
+    while (moveon == "no")
+    {
+        if (player2.Length < 3)
+        {
+            Console.WriteLine("The name cannot be shorter than 3 characters. Try again.");
+            Console.Write("Name: ");
+            player2 = Console.ReadLine();
+        }
+        else if (player2.Length > 10)
+        {
+            Console.WriteLine("The name cannot be longer than 10 characters. Try again.");
+            Console.Write("Name: ");
+            player2 = Console.ReadLine();
+        }
+        else
+        {
+            moveon = "yes";
+        }
+    }
+    // rundorna börjar här
+    while (hp1 > 0 && hp2 > 0)
     {
         // runda nr och HP kvar
         Console.WriteLine("");
-        round ++;
+        round++;
         Console.WriteLine($"Round {round}:");
-        Console.WriteLine($"\n{player1}'s HP: {hp1}");
-        Console.WriteLine($"{player2}'s HP: {hp2}\n");
+        Console.WriteLine($"{player1}'s HP: {hp1}");
+        Console.WriteLine($"{player2}'s HP: {hp2}");
         Console.ReadLine();
 
         // skada player 1
+        // .Next eftersom man stoppar in generator talet och får ut nästa tal, typ. 
+        // (20) för att ange vad max talet ska vara, annars blir det max värdet som en int kan ha
+        // om (20,100) = som minst 20, som max 100
         int damage1 = generator.Next(20);
         hp2 -= damage1;
         hp2 = Math.Max(0, hp2);
@@ -46,8 +99,8 @@ string player2 = Console.ReadLine();
     }
 
 
-// the end result
-if (hp1 <= 0)
+    // the end result
+    if (hp1 <= 0)
     {
         Console.WriteLine("");
         Console.WriteLine("GAME OVER!");
@@ -63,7 +116,7 @@ if (hp1 <= 0)
         Console.WriteLine($"The winner is {player1}!");
         Console.ReadLine();
         Console.WriteLine("Would you like to try again?");
-        tryagain = Console.ReadLine().ToLower();   
+        tryagain = Console.ReadLine().ToLower();
     }
     else if (hp1 <= 0 && hp2 <= 0)
     {
